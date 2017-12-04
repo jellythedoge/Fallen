5a2caea3019d46066a80d82922fef0a336b1b1a2
PuffingIn3D <jellythedoge@gmail.com>
2017-12-04 19:57:28 +1300

    Now writing a glow struct!

--- Fallen/API/SDK.cs
+++ Fallen/API/SDK.cs
@@ -675,7 +675,7 @@ namespace Fallen.API
 
         #endregion
 
-        #region SPECIAL
+        #region Structs && Other
 
         public static IntPtr Open_pHandel(string processName)
         {
@@ -695,6 +695,36 @@ namespace Fallen.API
                 return handle;
             }
         }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct GlowObject
+        {
+            [FieldOffset(0x00)]
+            public int EntityPointer;
+            [FieldOffset(0x4)]
+            public float r;
+            [FieldOffset(0x8)]
+            public float g;
+            [FieldOffset(0xC)]
+            public float b;
+            [FieldOffset(0x10)]
+            public float a;
+            [FieldOffset(0x14)]
+            public int jnk1;
+            [FieldOffset(0x18)]
+            public int jnk2;
+            [FieldOffset(0x1C)]
+            public float BloomAmount;
+            [FieldOffset(0x20)]
+            public int jnk3;
+
+            [FieldOffset(0x24)]
+            public bool m_bRenderWhenOccluded;
+            [FieldOffset(0x25)]
+            public bool m_bRenderWhenUnoccluded;
+            [FieldOffset(0x2C)]
+            public bool m_bFullBloom;
+        };
 
         #endregion
 
--- Fallen/Features/Glow.cs
+++ Fallen/Features/Glow.cs
@@ -3,6 +3,7 @@
 using Fallen.API;
 using Memory;
 using System.Threading;
+using System.Drawing;
 
 #endregion
 
@@ -25,42 +26,47 @@ namespace Fallen.Features
                     if (Arrays.Entity[i].m_iDormant == 1)
                         continue;
 
+                    SDK.GlowObject GlowObject = new SDK.GlowObject();
+
                     if (Arrays.Entity[i].m_iTeam != LocalPlayer.m_iTeam)
                     {
                         if (Settings.GlowEnemy.Enabled)
                         {
-                            MemoryManager.WriteMemory<float>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x10, Settings.GlowEnemy.Alpha / 255);
-                            MemoryManager.WriteMemory<float>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x4, Settings.GlowEnemy.Red / 255);
-                            MemoryManager.WriteMemory<float>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x8, Settings.GlowEnemy.Green / 255);
-                            MemoryManager.WriteMemory<float>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0xC, Settings.GlowEnemy.Blue / 255);
-                            MemoryManager.WriteMemory<bool>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x24, true);
-                            MemoryManager.WriteMemory<bool>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x25, false);
+                            GlowObject = MemoryManager.ReadMemory<SDK.GlowObject>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38);
 
-                            if (Settings.GlowEnemy.ChamsEnabled)
-                            {
-                                MemoryManager.WriteMemory<bool>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x2C, true);
-                            }
+                            GlowObject.r = Settings.GlowEnemy.Red / 255;
+                            GlowObject.g = Settings.GlowEnemy.Green / 255;
+                            GlowObject.b = Settings.GlowEnemy.Blue / 255;
+                            GlowObject.a = Settings.GlowEnemy.Alpha / 255;
+                            GlowObject.m_bRenderWhenOccluded = true;
+                            GlowObject.m_bRenderWhenUnoccluded = false;
+                            GlowObject.m_bFullBloom = Settings.GlowEnemy.ChamsEnabled;
+
+                            MemoryManager.WriteMemory<SDK.GlowObject>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38, GlowObject);
                         }
                     }
-                    else
+
+                    if (Arrays.Entity[i].m_iTeam == LocalPlayer.m_iTeam)
                     {
                         if (Settings.GlowTeam.Enabled)
                         {
-                            MemoryManager.WriteMemory<float>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x10, Settings.GlowTeam.Alpha / 255);
-                            MemoryManager.WriteMemory<float>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x4, Settings.GlowTeam.Red / 255);
-                            MemoryManager.WriteMemory<float>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x8, Settings.GlowTeam.Green / 255);
-                            MemoryManager.WriteMemory<float>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0xC, Settings.GlowTeam.Blue / 255);
-                            MemoryManager.WriteMemory<bool>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x24, true);
-                            MemoryManager.WriteMemory<bool>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x25, false);
+                            GlowObject = MemoryManager.ReadMemory<SDK.GlowObject>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38);
 
-                            if (Settings.GlowTeam.ChamsEnabled)
-                            {
-                                MemoryManager.WriteMemory<bool>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38 + 0x2C, true);
-                            }
+                            GlowObject.r = Settings.GlowTeam.Red / 255;
+                            GlowObject.g = Settings.GlowEnemy.Green / 255;
+                            GlowObject.b = Settings.GlowTeam.Blue / 255;
+                            GlowObject.a = Settings.GlowTeam.Alpha / 255;
+                            GlowObject.m_bRenderWhenOccluded = true;
+                            GlowObject.m_bRenderWhenUnoccluded = false;
+                            GlowObject.m_bFullBloom = Settings.GlowTeam.ChamsEnabled;
+
+                            MemoryManager.WriteMemory<SDK.GlowObject>(LocalPlayer.m_iGlowBase + Arrays.Entity[i].m_iGlowIndex * 0x38, GlowObject);
                         }
                     }
+
+
                 }
-                Thread.Sleep(30);
+                Thread.Sleep(1);
             }
         }
     }
